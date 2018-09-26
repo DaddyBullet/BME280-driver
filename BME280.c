138fd4cd2b705c5b630d40f75d0b04ded079f2e2
DaddyBullet <notna989898@gmail.com>
2018-09-26 10:13:06 +0300

    read and compensate functions imp;emented

--- BME280.c
+++ BME280.c
@@ -1,8 +1,10 @@
 #include "BME280.h"
 
-uint16_t BME280_temp_calib[3];
-uint16_t BME280_press_calib[9];
-uint16_t BME280_hum_calib[5];
+static uint16_t BME280_temp_calib[3];
+static uint16_t BME280_press_calib[9];
+static uint16_t BME280_hum_calib[5];
+
+static int32_t BME280_t_fine = 0;
 
 void BME280initallCompensationTable(uint8_t device_id)
 {
@@ -64,11 +66,103 @@ void BME280setMod(uint8_t device_id, uint8_t mod)
 
 uint32_t BME280readTemperatureData(uint8_t device_id)
 {
-  uint32_t raw_temp = BME280readDW(device_id, BME280_PRESS_MSB);
-  raw_temp >>= 4;
+  // TODO: resolution is not counted here
+  uint32_t raw_temp = BME280readDW(device_id, BME280_TEMP_MSB);
+  raw_temp >>= (4 + 8);
   if(raw_temp & (1 << 19))
   {
-    raw_temp |= (0b1111 << 20);
+    raw_temp |= (0xFFF << 20);
   }
   return raw_temp;
+}
+
+uint32_t BME280readPressureData(uint8_t device_id)
+{
+  // TODO: resolution is not counted here
+  uint32_t raw_press = BME280readDW(device_id, BME280_PRESS_MSB);
+  raw_press >>= (4 + 8);
+  return raw_temp;
+}
+
+uint16_t BME280readHumidityData(uint8_t device_id)
+{
+  uint16_t raw_hum = BME280readW(device_id, BME280_HUM_MSB);
+  return raw_hum;
+}
+
+float BME280readTemperature(uint8_t device_id)
+{
+  float real_temp = 80;
+  int32_t raw_temp = (int32_t)BME280readTemperatureData(device_id);
+  int32_t var1 = 0;
+  int32_t var2 = 0;
+
+  var1 = ((((raw_temp >> 3) - ((int32_t)BME280_temp_calib[0] << 1))) * \
+         ((int32_t)BME280_temp_calib[1])) >> 11;
+  var2 = (((((raw_temp >> 4) - ((int32_t)BME280_temp_calib[0])) * \
+         ((raw_temp >> 4) - ((int32_t)BME280_temp_calib[0]))) >> 12) * \
+         ((int32_t)BME280_temp_calib[2])) >> 14;
+
+  BME280_t_fine = var1 + var2;
+  real_temp = (float)((BME280_t_fine * 5 + 128) >> 8);
+  return  real_temp / 100.;
+}
+
+float BME280readPressure(uint8_t device_id)
+{
+  float real_pressure = -1;
+  uint32_t real_int_press = 0;
+  int32_t raw_press = (int32_t)BME280readPressureData(device_id);
+  int32_t var1 = 0;
+  int32_t var2 = 0;
+
+  var1 = (((int32_t)t_fine) >> 1) - (int32_t)64000;
+  var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * ((int32_t)BME280_press_calib[5]);
+  var2 += ((var1 * ((int32_t)BME280_press_calib[4])) << 1);
+  var2 = (var2 >> 2) + (((int32_t)BME280_press_calib[3]) << 16);
+  var1 = (((BME280_press[2] * (((var1 >> 2) * (var1 >> 2)) >> 13)) >> 3) + \
+         ((((int32_t)BME280_press_calib[1]) * var1) >> 1)) >> 18;
+  var1 = ((((32768 + var1)) * ((int32_t)BME280_press_calib[0])) >> 15);
+  if(!var1)
+  {
+    return 0;
+  }
+
+  real_int_press = (((uint32_t)(((int32_t)1048576) - raw_press) - (var2 >> 12))) * 3125;
+  if(real_int_press < 0x80000000)
+  {
+    real_int_press = (real_int_press << 1) / ((uint32_t)var1);
+  }
+  else
+  {
+    real_int_press = (real_int_press / (uint32_t)var1) * 2;
+  }
+
+  var1 = (((int32_t)BME280_press_calib[8]) * ((int32_t)(((real_int_press >> 3) * \
+         (real_int_press >> 3)) >> 13))) >> 12;
+  var2 = (((int32_t)(real_int_press >> 2)) * ((int32_t)BME280_press_calib[7])) >> 13;
+  real_int_press = (uint32_t)((int32_t)real_int_press + ((var1 + var2 + BME280_press_calib[6]) >> 4));
+  real_pressure = ((float)real_int_press) / 100.
+  return real_pressure;
+}
+
+float BME280readHumidity(uint8_t device_id)
+{
+  float real_hum = -1;
+  int32_t raw_hum = (int32_t)BME280readHumidityData(device_id);
+  int32_t v_x1_u32r = 0;
+
+  v_x1_u32r = (BME280_t_fine - (( int32_t)76800));
+  v_x1_u32r = (((((raw_hum << 14) - (((int32_t)BME280_hum_calib[3]) << 20) - \
+              (((int32_t)BME280_hum_calib[4]) * v_x1_u32r)) + \
+              ((int32_t)16384)) >> 15) * (((((((v_x1_u32r * ((BME280_hum_calib[5])) >> 10) * \
+              (((v_x1_u32r * ((int32_t)BME280_hum_calib[2])) >> 1) + \
+              ((int32_t)32768))) >> 10) + (int32_t)2097152)) * \
+              ((int32_t)BME280_hum_calib[1]) + 8192) >> 14));
+  v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) * \
+              ((int32_t)BME280_hum_calib[0])) >> 4));
+  v_x1_u32r = (v_x1_u32r < 0? 0 : v_x1_u32r);
+  v_x1_u32r = (v_x1_u32r > 419430400? 419430400 : v_x1_u32r);
+  real_hum = ((float)((uint32_t)(v_x1_u32r >> 12))) / 1024.;
+  return real_hum;  
 }
